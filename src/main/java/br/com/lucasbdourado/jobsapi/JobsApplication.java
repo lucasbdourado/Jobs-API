116601ca7a3265f32cf1cbad2b7d3b6543dfd16c
Lucas Dourado <douradoblucas@gmail.com>
2023-09-04 04:29:03 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/com/lucasbdourado/jobsapi/JobsApplication.java
@@ -0,0 +1,9 @@
+package br.com.lucasbdourado.jobsapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class JobsApplication extends Application {
+
+}
